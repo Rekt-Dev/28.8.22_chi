5f6067a4421b2c0ea0a9bf30046c9a2ec319f31b
rektDev <rektdevvv@gmail.com>
2022-08-28 14:56:59 +0000

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -40,17 +40,17 @@ export let Card = (props: any) => {
           <ul>
             {}
             <a href="./GoToArtist">
-              <p className={("truncate", "")} onClick={goToArtist}>
+              <p className={("truncate")} onClick={goToArtist}>
                 Title: {truncate(props.item.title) || "Artist name"}
               </p>
             </a>{" "}
             <a href="./GoToArtist">
-              <p className={("truncate", "alignLeft")} onClick={goToArtist}>
+              <p className={("truncate")} onClick={goToArtist}>
                 Artist: {truncate(props.item.artist_title) || "Title Name"}
               </p>
             </a>
             <a href="./GoToArtist">
-              <p className={("truncate", "alignLeft")} onClick={goToArtist}>
+              <p className={("truncate")} onClick={goToArtist}>
                 Origin: {props.item.place_of_origin || "origin"}
               </p>
             </a>
